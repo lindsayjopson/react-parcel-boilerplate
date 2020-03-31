5e6d04c6fd055104dbc489fb36518bf2659cf9a6
Lindsay Jopson <lindsayjopson@gmail.com>
2020-03-31 15:53:35 +1300

    init project, workin nice 🚀

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-parcel-boilerplate
+
 A boilerplate project using React &amp; Parcel
+
+Referenced in my [Medium post]() for usage instructions.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+const App = () => <>react-parcel-boilerplate</>;
+
+render(<App />, document.getElementById("root"));
